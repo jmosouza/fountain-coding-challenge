bc614b3fd01caddcbd68d31e5f208f1717c3ed6c
João Marcelo Souza <joaomarcelo9393@gmail.com>
2017-10-24 18:04:54 -0300

    Create applicants

--- hire.rb
+++ hire.rb
@@ -1,37 +1,81 @@
-# Process hiring stages and candidates.
-class HireService
-  COMMAND_MAP = {
-    'DEFINE' => :define_stages,
-    'CREATE' => :create_applicant,
-    'ADVANCE' => :define_applicant,
-    'DECIDE' => :define_applicant,
-    'STATS' => :show_stats
-  }.freeze
+require "byebug"
 
-  VALID_STAGES =
-    %w[ManualReview PhoneInterview BackgroundCheck DocumentSigning].freeze
+module Hire
+  # A job applicant
+  class Applicant
+    attr_accessor :email
+    attr_accessor :stage
 
-  INVALID_COMMAND_MESSAGE = 'This command is invalid.'.freeze
+    def initialize(email:, stage:)
+      @email = email
+      @stage = stage
+    end
 
-  def take_command(cmd)
-    cmd_name, *cmd_args = cmd.split
-    method = COMMAND_MAP[cmd_name]
-    method ? send(method, cmd_args) : puts(INVALID_COMMAND_MESSAGE)
+    # Two instances refer to the same applicant if both have same email.
+    # DEVNOTE: Avoid `case` here, it doesn't work with `Object#class`.
+    def ==(other)
+      if other.is_a? Applicant
+        @email == other.email
+      elsif other.is_a? String
+        @email == other
+      else
+        false
+      end
+    end
   end
 
-  private
+  class Service
+    COMMAND_MAP = {
+      'DEFINE' => :define_stages,
+      'CREATE' => :create_applicant,
+      'ADVANCE' => :define_applicant,
+      'DECIDE' => :define_applicant,
+      'STATS' => :show_stats
+    }.freeze
 
-  # Stages passed as argument must be valid.
-  # Those that aren't will be ignored.
-  def define_stages(stages)
-    @stages = VALID_STAGES & stages
+    VALID_STAGES =
+      %w[ManualReview PhoneInterview BackgroundCheck DocumentSigning].freeze
+
+    INVALID_COMMAND_MESSAGE = 'Invalid command'.freeze
+    DUPLICATE_APPLICANT_MESSAGE = 'Duplicate applicant'.freeze
+
+    def initialize
+      @stages = []
+      @applicants = []
+    end
+
+    # Call the method corresponding to a command.
+    def parse_command(cmd)
+      cmd_name, *cmd_args = cmd.split
+      if (method = COMMAND_MAP[cmd_name])
+        send(method, *cmd_args)
+      else
+        puts INVALID_COMMAND_MESSAGE
+      end
+    end
+
+    private
+
+    # Define the stages in the hiring process. The available stages are
+    # `ManualReview PhoneInterview BackgroundCheck DocumentSigning`.
+    def define_stages(*stages)
+      @stages = VALID_STAGES & stages
+    end
+
+    # Create an applicant with the specified email address.
+    # Check if the applicant is already in the system before creating a new one.
+    def create_applicant(email)
+      if @applicants.include? email
+        puts DUPLICATE_APPLICANT_MESSAGE
+      else
+        @applicants << Applicant.new(email: email, stage: @stages.first)
+      end
+    end
   end
 end
 
-hire = HireService.new
+hire = Hire::Service.new
 
 while (cmd = gets.strip) && !cmd.empty?
-  hire.take_command(cmd)
+  hire.parse_command(cmd)
 end
-
-puts hire.stages
