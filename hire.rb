2c91b317780499d7f15b7f48209b0db7e071dec3
João Marcelo Souza <joaomarcelo9393@gmail.com>
2017-10-25 10:29:32 -0300

    Pre-safe-operator ruby support

--- hire.rb
+++ hire.rb
@@ -133,6 +133,6 @@ file_input = File.new('input.txt', 'r')
 file_output = File.new('output.txt', 'w')
 hire = Hire::Service.new(file_output)
 
-while (cmd = file_input.gets&.strip)
-  hire.parse_command(cmd)
+while (cmd = file_input.gets)
+  hire.parse_command(cmd.strip)
 end
